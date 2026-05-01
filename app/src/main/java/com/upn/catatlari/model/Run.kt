c9f24d67874bd9108ac7ecba92715a6e59361f37
SAKA <suryanataradit@gmail.com>
2026-05-01 20:29:42 +0700

    initial commit - fitur update

---
+++ app/src/main/java/com/upn/catatlari/model/Run.kt
@@ -0,0 +1,15 @@
+package com.upn.catatlari.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = "runs")
+data class Run(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val runDate: String,
+    val runDistance: Int,
+    val runDuration: Int
+) : Parcelable
---
+++ app/src/main/java/com/upn/catatlari/view/LoginFragment.kt
@@ -0,0 +1,50 @@
+package com.upn.catatlari.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.upn.catatlari.databinding.FragmentLoginBinding
+import com.upn.catatlari.model.User
+import com.upn.catatlari.viewmodel.AuthViewModel
+
+class LoginFragment : Fragment() {
+
+    private lateinit var loginBinding: FragmentLoginBinding
+    private val authViewModel: AuthViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        loginBinding = FragmentLoginBinding.inflate(inflater, container, false)
+        return loginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginBinding.buttonLogin.setOnClickListener {
+            val emailUser = loginBinding.etEmail.text.toString().trim()
+            val passwordUser = loginBinding.etPassword.text.toString()
+
+            if (emailUser.isEmpty() || passwordUser.isEmpty()) {
+                Toast.makeText(requireContext(), "Silahkan masukkan email/password, bro!", Toast.LENGTH_SHORT).show()
+            } else {
+                authViewModel.login(emailUser, passwordUser)
+            }
+        }
+
+        authViewModel.loginStatus.observe(viewLifecycleOwner) { user ->
+            if (user != null) {
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                intent.putExtra("user", user)
+                startActivity(intent)
+                requireActivity().finish()
+            } else {
+                Toast.makeText(requireContext(), "Email atau password salah!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
